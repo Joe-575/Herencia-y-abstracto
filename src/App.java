2d6056c6967878108097630bcf05233c8f96e545
Joe-575 <medinajoel814@gmail.com>
2024-11-26 21:42:50 -0500

    update herencia

--- src/App.java
+++ src/App.java
@@ -7,11 +7,13 @@ public class App {
         Padre padre = new Padre();
         Hijo hijo = new Hijo();
 
+        //enviar datos
         padre.setNombre(" Mario");
         padre.setEdad(55);
         hijo.setNombre("Juan CArlos");
         hijo.setEdad(23);
 
+        //recibir y mostar los datos
         System.out.println("El padre se llama "+padre.getNombre() + " y tiene "+padre.getEdad() + " años");
         System.out.println("El hijo se llama "+hijo.getNombre()+" y tiene "+hijo.getEdad()+" años");
         
